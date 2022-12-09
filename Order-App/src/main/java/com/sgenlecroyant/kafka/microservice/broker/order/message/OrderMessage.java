4264b052643869402a9a4a73711ca699811b9e59
sgenlecroyant <sgenlecroyant@gmail.com>
2022-12-09 22:43:28 +0200

    created OrderMessage

--- Order-App/src/main/java/com/sgenlecroyant/kafka/microservice/broker/order/message/OrderMessage.java
+++ Order-App/src/main/java/com/sgenlecroyant/kafka/microservice/broker/order/message/OrderMessage.java
@@ -1,29 +1,44 @@
 package com.sgenlecroyant.kafka.microservice.broker.order.message;
 
-import java.util.UUID;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import com.sgenlecroyant.kafka.microservice.order.builder.OrderRequestBuilder;
+import com.sgenlecroyant.kafka.microservice.entity.Order;
+import com.sgenlecroyant.kafka.microservice.entity.OrderItem;
+import com.sgenlecroyant.kafka.microservice.order.builder.OrderMessageBuilder;
 
 public class OrderMessage {
 
-	private String id;
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+	private String orderId;
 	private String name;
 	private Double price;
 	private Integer quantity;
+	private String creditCardNumber;
+	private String location;
+	private String itemId;
 
-	public OrderMessage(OrderRequestBuilder orderRequestBuilder) {
-		this.id = UUID.randomUUID().toString().replaceAll("-", "");
-		this.name = orderRequestBuilder.getName();
-		this.price = orderRequestBuilder.getPrice();
-		this.quantity = orderRequestBuilder.getQuantity();
+	public OrderMessage(String orderId, String name, Double price, Integer quantity, String creditCardNumber,
+			String location, String itemId) {
+		this.orderId = orderId;
+		this.name = name;
+		this.price = price;
+		this.quantity = quantity;
+		this.creditCardNumber = creditCardNumber;
+		this.location = location;
+		this.itemId = itemId;
+		this.logger.info("{}", this);
 	}
 
-	public String getId() {
-		return id;
-	}
-
-	public void setId(String id) {
-		this.id = id;
+	public OrderMessage(OrderMessageBuilder orderMessageBuilder) {
+		this.orderId = orderMessageBuilder.getOrderId();
+		this.name = orderMessageBuilder.getName();
+		this.price = orderMessageBuilder.getPrice();
+		this.quantity = orderMessageBuilder.getQuantity();
+		this.creditCardNumber = orderMessageBuilder.getCreditCardNumber();
+		this.location = orderMessageBuilder.getLocation();
+		this.itemId = orderMessageBuilder.getItemId();
 	}
 
 	public String getName() {
@@ -50,9 +65,46 @@ public class OrderMessage {
 		this.quantity = quantity;
 	}
 
+	public String getCreditCardNumber() {
+		return creditCardNumber;
+	}
+
+	public void setCreditCardNumber(String creditCardNumber) {
+		this.creditCardNumber = creditCardNumber;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
+	public String getOrderId() {
+		return orderId;
+	}
+
+	public void setOrderId(String orderId) {
+		this.orderId = orderId;
+	}
+
+	public String getItemId() {
+		return itemId;
+	}
+
+	public void setItemId(String itemId) {
+		this.itemId = itemId;
+	}
+
+	public static OrderMessageBuilder newBuilder(OrderItem orderItem, Order order) {
+		return new OrderMessageBuilder(orderItem, order);
+	}
+
 	@Override
 	public String toString() {
-		return "OrderMessage [id=" + id + ", name=" + name + ", price=" + price + ", quantity=" + quantity + "]";
+		return "OrderMessage [orderId=" + orderId + ", name=" + name + ", price=" + price + ", quantity=" + quantity
+				+ ", creditCardNumber=" + creditCardNumber + ", location=" + location + ", itemId=" + itemId + "]";
 	}
 
 }
