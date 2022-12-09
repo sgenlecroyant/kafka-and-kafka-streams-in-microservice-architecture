8b8abe62ba962f23a40ea20dc4c3704cc9c9e6c2
sgenlecroyant <sgenlecroyant@gmail.com>
2022-12-09 17:45:48 +0200

    added parent module

---
+++ src/main/java/com/sgenlecroyant/kafka/microservice/KafkaMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.sgenlecroyant.kafka.microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sgenlecroyant/kafka/microservice/KafkaMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sgenlecroyant.kafka.microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
