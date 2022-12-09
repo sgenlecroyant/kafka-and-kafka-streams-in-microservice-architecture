8644f1c100108455ce3baddcbbcf067d207c2eb0
sgenlecroyant <sgenlecroyant@gmail.com>
2022-12-09 22:43:13 +0200

    refactored OrderController

--- Order-App/src/main/java/com/sgenlecroyant/kafka/microservice/api/server/OrderController.java
+++ Order-App/src/main/java/com/sgenlecroyant/kafka/microservice/api/server/OrderController.java
@@ -3,7 +3,6 @@ package com.sgenlecroyant.kafka.microservice.api.server;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.kafka.support.serializer.JsonDeserializer;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.sgenlecroyant.kafka.microservice.api.server.request.OrderRequest;
 import com.sgenlecroyant.kafka.microservice.api.server.response.OrderResponse;
-import com.sgenlecroyant.kafka.microservice.broker.order.message.OrderMessage;
+import com.sgenlecroyant.kafka.microservice.entity.Order;
 import com.sgenlecroyant.kafka.microservice.service.OrderService;
 
 @RestController
@@ -19,7 +18,6 @@ import com.sgenlecroyant.kafka.microservice.service.OrderService;
 public class OrderController {
 
 	private final OrderService orderService;
-	
 
 	public OrderController(OrderService orderService) {
 		this.orderService = orderService;
@@ -27,10 +25,9 @@ public class OrderController {
 
 	@PostMapping(path = "/", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<OrderResponse> createOrder(@RequestBody OrderRequest orderRequest) {
-		OrderMessage orderMessage = this.orderService.sendOrderToKafka(orderRequest);
-		OrderResponse orderResponse = OrderResponse.createOrderResponse(orderMessage);
 		
-//		JsonDeserializer<T>
+		Order order = this.orderService.saveAndSend(orderRequest);
+		OrderResponse orderResponse = OrderResponse.createOrderResponse(order);
 		return ResponseEntity.status(HttpStatus.OK).body(orderResponse);
 	}
 
