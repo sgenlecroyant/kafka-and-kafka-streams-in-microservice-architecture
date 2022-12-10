0ce4e34267117733e0c2f2cf5f56f38ec925dd5a
sgenlecroyant <sgenlecroyant@gmail.com>
2022-12-10 17:15:44 +0200

    moved OrderRepo and OrderItem repo to the right package

--- Order-App/src/main/java/com/sgenlecroyant/kafka/microservice/service/repository/OrderItemRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sgenlecroyant.kafka.microservice.service.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.sgenlecroyant.kafka.microservice.entity.OrderItem;
-
-public interface OrderItemRepository extends JpaRepository<OrderItem, String>{
-
-}
--- Order-App/src/main/java/com/sgenlecroyant/kafka/microservice/service/repository/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sgenlecroyant.kafka.microservice.service.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.sgenlecroyant.kafka.microservice.entity.Order;
-
-public interface OrderRepository extends JpaRepository<Order, String> {
-
-}
