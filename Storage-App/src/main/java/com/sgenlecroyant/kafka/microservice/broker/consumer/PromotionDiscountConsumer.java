73476adff00ba4036a6e3c24994184b2350126a9
sgenlecroyant <sgenlecroyant@gmail.com>
2022-12-15 18:49:12 +0200

    removed unused imports

--- Storage-App/src/main/java/com/sgenlecroyant/kafka/microservice/broker/consumer/PromotionDiscountConsumer.java
+++ Storage-App/src/main/java/com/sgenlecroyant/kafka/microservice/broker/consumer/PromotionDiscountConsumer.java
@@ -1,9 +1,11 @@
 package com.sgenlecroyant.kafka.microservice.broker.consumer;
 
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaHandler;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.Acknowledgment;
 import org.springframework.stereotype.Service;
 
 import com.sgenlecroyant.kafka.microservice.broker.order.message.DiscountMessage;
