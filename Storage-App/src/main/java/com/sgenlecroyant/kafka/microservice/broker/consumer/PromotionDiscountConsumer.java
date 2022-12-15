dee900c40fbdf252b85c2ad24fc0870e179e41de
sgenlecroyant <sgenlecroyant@gmail.com>
2022-12-15 16:32:56 +0200

    created listeners for Discount and Promotion with KafkaHandler to stream diffirent messages from a single topic

---
+++ Storage-App/src/main/java/com/sgenlecroyant/kafka/microservice/broker/consumer/PromotionDiscountConsumer.java
@@ -0,0 +1,27 @@
+package com.sgenlecroyant.kafka.microservice.broker.consumer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaHandler;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+import com.sgenlecroyant.kafka.microservice.broker.order.message.DiscountMessage;
+import com.sgenlecroyant.kafka.microservice.broker.order.message.PromotionMessage;
+
+@Service
+@KafkaListener(topics = "promotions-topic")
+public class PromotionDiscountConsumer {
+
+	private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+	@KafkaHandler
+	public void streamPromotions(PromotionMessage promotionMessage) {
+		this.logger.info("promotion message: {}", promotionMessage);
+	}
+
+	@KafkaHandler
+	public void streamDiscounts(DiscountMessage discountMessage) {
+		this.logger.info("discount message: {}", discountMessage);
+	}
+}
