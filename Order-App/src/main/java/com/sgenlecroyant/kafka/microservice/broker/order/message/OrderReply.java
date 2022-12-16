e6899154b64bf87ad955546a1d13984ec3c642d0
sgenlecroyant <sgenlecroyant@gmail.com>
2022-12-16 11:10:20 +0200

    defined OrderReply message structure

---
+++ Order-App/src/main/java/com/sgenlecroyant/kafka/microservice/broker/order/message/OrderReply.java
@@ -0,0 +1,24 @@
+package com.sgenlecroyant.kafka.microservice.broker.order.message;
+
+public class OrderReply {
+
+	private String message;
+
+	public OrderReply(String message) {
+		this.message = message;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	@Override
+	public String toString() {
+		return "OrderReply [message=" + message + "]";
+	}
+
+}
