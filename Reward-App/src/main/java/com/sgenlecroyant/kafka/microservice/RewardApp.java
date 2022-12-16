0d3cde059b77cb677c106038e96fadb570ea4a15
sgenlecroyant <sgenlecroyant@gmail.com>
2022-12-16 11:09:25 +0200

    created the entrypoint class for REWARD service

---
+++ Reward-App/src/main/java/com/sgenlecroyant/kafka/microservice/RewardApp.java
@@ -0,0 +1,13 @@
+package com.sgenlecroyant.kafka.microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RewardApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RewardApp.class, args);
+	}
+
+}
