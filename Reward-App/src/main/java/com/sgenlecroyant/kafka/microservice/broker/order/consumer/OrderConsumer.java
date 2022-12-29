fd0a1d92a6f244b16f8fc12859c4d5b882e79f0c
sgenlecroyant <sgenlecroyant@gmail.com>
2022-12-29 11:32:33 +0200

    cleaned up imports

--- Reward-App/src/main/java/com/sgenlecroyant/kafka/microservice/broker/order/consumer/OrderConsumer.java
+++ Reward-App/src/main/java/com/sgenlecroyant/kafka/microservice/broker/order/consumer/OrderConsumer.java
@@ -9,14 +9,14 @@ import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
 import org.springframework.kafka.listener.AcknowledgingConsumerAwareMessageListener;
 import org.springframework.kafka.support.Acknowledgment;
 import org.springframework.kafka.support.serializer.JsonSerializer;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Component;
 
 import com.fasterxml.jackson.core.type.TypeReference;
