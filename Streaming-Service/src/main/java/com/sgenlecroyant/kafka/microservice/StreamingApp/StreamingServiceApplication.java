28a6a321bf2e5c6249667ad7e52689e8941a87df
sgenlecroyant <sgenlecroyant@gmail.com>
2023-01-02 13:46:36 +0200

    set up the Data Streaming App

---
+++ Streaming-Service/src/main/java/com/sgenlecroyant/kafka/microservice/StreamingApp/StreamingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sgenlecroyant.kafka.microservice.StreamingApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreamingServiceApplication.class, args);
+	}
+
+}
---
+++ Streaming-Service/src/test/java/com/sgenlecroyant/kafka/microservice/StreamingApp/StreamingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sgenlecroyant.kafka.microservice.StreamingApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
